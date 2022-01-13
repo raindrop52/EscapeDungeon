f7954d56da143bea24e664456f0e358a0ba9d124
raindrop52 <93302266+raindrop52@users.noreply.github.com>
2022-01-13 14:03:45 +0900

    임시 코드

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace EscapeGame
+{
+    public class CameraManager : MonoBehaviour
+    {
+        public static CameraManager _inst;
+
+        
+
+        void Awake()
+        {
+            _inst = this;
+        }
+
+        void Start()
+        {
+
+        }
+
+        
+        void Update()
+        {
+
+        }
+    }
+}
