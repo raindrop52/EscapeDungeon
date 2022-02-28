1d4e5d2c832dfcc9cf0d77a93eee99c408d4ca5f
raindrop52 <93302266+raindrop52@users.noreply.github.com>
2022-02-28 13:10:52 +0900

    22/02/28 - 4 작업사항
    탈출
     - 마비 함정 추가

--- Assets/Scripts/Escape/Traps/Foot/Poison/Paralysis.cs
+++ Assets/Scripts/Escape/Traps/Foot/Poison/Paralysis.cs
@@ -6,14 +6,31 @@ namespace EscapeGame
 {
     public class Paralysis : Poison
     {
+        Player_Control _pc = null;
+
         protected override void ExecutePoison(Player player)
         {
+            if (_pc == null)
+                _pc = player.GetComponent<Player_Control>();
+
+            if (_pc != null)
+            {
+                // 플레이어 이동 제한
+                _pc.enabled = false;
+            }
+
             base.ExecutePoison(player);
         }
 
         protected override void ClosePoison()
         {
             base.ClosePoison();
+
+            if (_pc != null)
+            {
+                // 플레이어 이동 제한 해제
+                _pc.enabled = true;
+            }
         }
     }
 }
